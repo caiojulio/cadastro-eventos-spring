e79f9b64ecafc858d090b6728e7038f27c8f6e8f
caiojulio <caiojuliosilv@gmail.com>
2024-01-06 02:01:41 -0300

    Criação e configuração de repositório para utilização de JPÁ, modificação do template index.html para apresentar dados do banco de dados, criação de métodos para busca e inserção de dados no banco

---
+++ eventapp/src/main/java/com/eventapp/eventapp/repository/EventoRepository.java
@@ -0,0 +1,9 @@
+package com.eventapp.eventapp.repository;
+
+import com.eventapp.eventapp.models.Evento;
+import org.springframework.data.repository.CrudRepository;
+
+public interface EventoRepository extends CrudRepository<Evento, Integer> {
+
+
+}
