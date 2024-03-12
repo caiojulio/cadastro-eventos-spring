ac9146cf76ba7b50f5f04f0a91dd285ef6009f83
caiojulio <caiojuliosilv@gmail.com>
2024-03-12 19:09:04 -0300

    reorganizando projeto

--- eventapp/src/main/java/com/eventapp/eventapp/EventappApplication.java
+++ eventapp/src/main/java/com/eventapp/eventapp/EventappApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class EventappApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(EventappApplication.class, args);
 	}
--- eventapp/src/main/java/com/eventapp/eventapp/repository/EventoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.eventapp.eventapp.repository;
-
-import com.eventapp.eventapp.models.Evento;
-import org.springframework.data.repository.CrudRepository;
-
-public interface EventoRepository extends CrudRepository<Evento, Integer> {
-
-
-}
